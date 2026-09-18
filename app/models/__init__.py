61e7b657c8bf1a1823108fb23376793a2bde730a
Shadrack Mason <shadrackmason@masonautohub.com>
2026-09-18 12:41:16 +0300

    feat(models): register all SQLAlchemy models in package init

---
+++ app/models/__init__.py
@@ -0,0 +1,10 @@
+from app.models.car import Car
+from app.models.mechanic import Mechanic
+from app.models.service import ServiceBooking
+from app.models.user import User
+from app.models.spare_part import SparePart
+from app.models.import_request import ImportRequest
+from app.models.saved_car import SavedCar
+from app.models.cart_item import CartItem
+
+__all__ = ["Car", "Mechanic", "ServiceBooking", "User", "SparePart", "ImportRequest", "SavedCar", "CartItem"]
